9c64dc3fc3a0eb85407dfd79e16152096effb5cb
Paulo Ferreira <pf@sourcenotes.org>
2022-09-08 16:43:57 +0100

    Cleaned up Action Listener

--- src/listeners/action-start.ts
+++ src/listeners/action-start.ts
@@ -17,6 +17,11 @@ import utils from '../utilities.js';
 import type { Listener } from './listener.js';
 
 async function processEmailAction(actions: string[], message: any): Promise<any> {
+  expect(broker != null, 'Invalid Broker Object').to.be.true;
+
+  // Change Message Type to (email:...)
+  message.type = actions.join(':')
+
   const publication = await broker.publish('email-in', message);
   publication
     .on('success', (messageId) => {
@@ -27,38 +32,37 @@ async function processEmailAction(actions: string[], message: any): Promise<any>
     })
 }
 
-async function processMessage(message: any): Promise<string> {
-  console.log(message)
-
-  // Verify Minimum Requirements for Message
-  expect(message, 'Invalid Message').to.be.an('object');
-  expect(message.version, 'Invalid Value for "version"').to.be.a('number').that.is.gt(0);
-  expect(message.id, 'Invalid Value for "id"').to.be.a('string').that.is.not.empty;
-  expect(message.type, 'Invalid Value for "type"').to.be.a('string').that.is.not.empty;
-
-  console.info(`Processing Message [${message.version}:${message.id}]`);
-  const type: string | null = utils.strings.nullOnEmpty(message.type.trim());
-  expect(type, '"type" has no value').not.to.be.null;
-
-  // Parse Action Type
-  const actions: string[] = (<string>type).split(':');
-  expect(actions.length > 1, `[${type}] is not a valid action`).to.be.true;
-  expect(actions[0] === 'action', `[${type}] is not a valid action`).to.be.true;
-
-  switch (actions[1]) {
+async function processAction(actions: string[], message: any): Promise<string> {
+  switch (actions[0]) {
     case 'email': // Email Action
-      return processEmailAction(actions.slice(2), message);
+      return processEmailAction(actions, message);
     default:
       throw new Error('"type" has an unrecognized value');
   }
 }
 
 async function messageListener(message: Message, content: any, ackOrNack: rascal.AckOrNack) {
-  expect(broker != null, 'Invalid Broker Object').to.be.true;
 
   try {
-    const id: string = await processMessage(content)
-    console.info(`Message Sent [${id}]`);
+    // Verify Minimum Requirements for Message
+    expect(content, 'Invalid Message').to.be.an('object');
+    expect(content.version, 'Invalid Value for "version"').to.be.a('number').that.is.gt(0);
+    expect(content.id, 'Invalid Value for "id"').to.be.a('string').that.is.not.empty;
+    expect(content.type, 'Invalid Value for "type"').to.be.a('string').that.is.not.empty;
+    console.log(content)
+
+    // Extract Message Type
+    console.info(`Processing Message [${content.version}:${content.id}]`);
+    const type: string | null = utils.strings.nullOnEmpty(content.type.trim());
+    expect(type, '"type" has no value').not.to.be.null;
+
+    // Parse Action Type
+    const actions: string[] = (<string>type).split(':');
+    expect(actions.length > 1, `[${type}] is not a valid action`).to.be.true;
+    expect(actions[0] === 'action', `[${type}] is not a valid action`).to.be.true;
+
+    const id: string = await processAction(actions.slice(1), content)
+    console.info(`Action Processed [${content.id}-${type}]`);
     ackOrNack();
   } catch (e: any) {
     console.error(e);
